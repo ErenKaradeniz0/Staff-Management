e632cc0e097d74603890b69650768838f3f95169
Enes Sağlam <59392488+enessaglam0180@users.noreply.github.com>
2024-05-21 02:09:04 +0300

    GroupAdmin index.htmlcs teki tabloya veri tabanı bağlandı ve tablo veri tabanından çekilir hale getirildi.system admin page

--- Staff Management/Controllers/GroupAdminController.cs
+++ Staff Management/Controllers/GroupAdminController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Staff_Management.Models; // Adjust namespace as necessary
 
 namespace Staff_Management.Controllers
 {
     public class GroupAdminController : Controller
     {
-        // GET: GroupAdmin
+
+
+
+        // GET: 
         public ActionResult Index()
         {
-            return View();
+            GroupAdminViewModel TaskList = new GroupAdminViewModel();
+            return View(TaskList.ListTasks());
+
+            //return View(ListTasks());
+
+            //return View();
         }
 
         public ActionResult Tasks() {
---
+++ Staff Management/Models/GroupAdminViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace Staff_Management.Models
+{
+    public class GroupAdminViewModel
+    {
+        public string TaskTitle { get; set; }
+        public byte TaskStatus { get; set; }
+        public string UserName { get; set; }
+        public string UserSurname { get; set; }
+
+        public List<GroupAdminViewModel> ListTasks()
+        {
+            using (var ctx = new DbContextViewModel())
+            {
+                var query = from task in ctx.Tasks
+                            join user in ctx.Users
+                            on task.StaffId equals user.UserId
+                            select new GroupAdminViewModel
+                            {
+                                TaskStatus = task.Status,
+                                TaskTitle = task.Title,
+                                UserName = user.Name,
+                                UserSurname = user.Surname
+                            };
+
+                return query.ToList();
+            }
+        }
+
+    }
+}

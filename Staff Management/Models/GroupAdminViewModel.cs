ed6ce8b86875490f81d55adda6da6517e7da8521
Deniz <Deniz@DESKTOP-8VVON23>
2024-05-25 16:28:52 +0300

    query de değişiklik ama tam olarak o sayfadaki iş  tamamlanmadı

--- Staff Management/Models/GroupAdminViewModel.cs
+++ Staff Management/Models/GroupAdminViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI.WebControls;
 
@@ -8,6 +9,7 @@ namespace Staff_Management.Models
 {
     public class GroupAdminViewModel
     {
+        public int TaskId { get; set; }
         public string TaskTitle { get; set; }
         public byte TaskStatus { get; set; }
         public string UserName { get; set; }
@@ -17,11 +19,12 @@ namespace Staff_Management.Models
         {
             using (var ctx = new DbContextViewModel())
             {
-                var query = from task in ctx.Tasks
-                            join user in ctx.Users
-                            on task.StaffId equals user.UserId
+                var query = from assignment in ctx.Assignments
+                            join user in ctx.Users on assignment.StaffId equals user.UserId
+                            join task in ctx.Tasks on user.UserId equals task.StaffId
                             select new GroupAdminViewModel
                             {
+                                TaskId = task.TaskId,
                                 TaskStatus = task.Status,
                                 TaskTitle = task.Title,
                                 UserName = user.Name,

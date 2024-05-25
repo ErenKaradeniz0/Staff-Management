148087f175ba49c1597f452d1f1e4db41735fd57
Eren Karadeniz <akincilareren@gmail.com>
2024-05-25 17:28:00 +0300

    Group Admin view own tasks

--- Staff Management/Controllers/GroupAdminController.cs
+++ Staff Management/Controllers/GroupAdminController.cs
@@ -8,18 +8,43 @@ using Staff_Management.Models; // Adjust namespace as necessary
 
 namespace Staff_Management.Controllers
 {
+
     public class GroupAdminController : Controller
     {
-      
+        private readonly DbContextViewModel _context;
+
+        public GroupAdminController()
+        {
+            _context = new DbContextViewModel();
+        }
+
         // GET: 
         public ActionResult Index()
         {
-            GroupAdminViewModel TaskList = new GroupAdminViewModel();
-            return View(TaskList.ListTasks());
+            int UserID = Convert.ToInt32(Session["UserID"]);
 
+            // Query to fetch the data with the filter condition
+            var query = from assignment in _context.Assignments
+                        join user in _context.Users on assignment.StaffId equals user.UserId
+                        join task in _context.Tasks on user.UserId equals task.StaffId
+                        where assignment.GroupAdminId == UserID // Filter condition
+                        select new GroupAdminViewModel
+                        {
+                            TaskId = task.TaskId,
+                            TaskStatus = task.Status,
+                            TaskTitle = task.Title,
+                            UserName = user.Name,
+                            UserSurname = user.Surname
+                        };
 
-            //return View();
+            // Convert the query result to a list
+            List<GroupAdminViewModel> TaskList = query.ToList();
+
+            // Pass the list to the view
+            return View(TaskList);
         }
+
+
 
         public ActionResult Tasks() {
             return View();

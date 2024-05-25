2a0a604e54002c4ab23fb29cbd840c74ace815f3
Eren Karadeniz <akincilareren@gmail.com>
2024-05-25 21:00:03 +0300

    Layout  Quit, Profile button for Staff Page Tasks Model  Fixed
    CompleteTask
    Profile navbar Deleted
    profile form field required
    salary read-only
    
    reportIssue connected to database
    TaskDetails connected to database

--- Staff Management/Controllers/StaffController.cs
+++ Staff Management/Controllers/StaffController.cs
@@ -1,6 +1,7 @@
 ﻿using Staff_Management.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,17 +22,99 @@ namespace Staff_Management.Controllers
             var tasks = _context.Tasks.Where(t => t.StaffId == UserId).ToList();
             return View(tasks);
         }
-        public ActionResult TaskDetails()
+        
+        [HttpGet]
+        public ActionResult TaskDetails(int id)
         {
-            return View();
+            var task = _context.Tasks.FirstOrDefault(t => t.TaskId == id);
+
+            if (task == null)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+
+            return View(task);
+        }
+
+        [HttpPost]
+        public ActionResult TaskDetails(Tasks taskModel)
+        {
+            var existingTask = _context.Tasks.Find(taskModel.TaskId);
+
+            if (existingTask == null)
+            {
+                // Task not found
+                return RedirectToAction("Index", "Error");
+            }
+
+            existingTask.Comment = taskModel.Comment;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                // Handle concurrency conflict
+                return View("ConcurrencyError", ex);
+            }
+
+            // Redirect to the TaskDetails action with the ID of the updated task
+            return RedirectToAction("TaskDetails", new { id = existingTask.TaskId });
         }
         public ActionResult Profile()
         {
             return View();
         }
-        public ActionResult ReportIssue()
+
+        [HttpPost]
+        public ActionResult UpdateTaskStatus(int taskId)
         {
-            return View();
+            var task = _context.Tasks.Find(taskId);
+            if (task != null)
+            {
+                task.Status = 2;
+                _context.SaveChanges();
+                return Json(new { success = true });
+            }
+            return Json(new { success = false });
+        }
+        [HttpGet]
+        public ActionResult ReportIssue(int id)
+        {
+            var task = _context.Tasks.FirstOrDefault(t => t.TaskId == id);
+
+            if (task == null)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+
+            return View(task);
+        }
+
+        [HttpPost]
+        public ActionResult ReportIssue(Tasks taskModel)
+        {
+            var existingTask = _context.Tasks.Find(taskModel.TaskId);
+
+            if (existingTask == null)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+
+
+            existingTask.Comment = taskModel.Comment;
+            existingTask.Status = taskModel.Status;
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return View("ConcurrencyError", ex);
+            }
+
+            return RedirectToAction("ReportIssue", new { id = existingTask.TaskId });
         }
     }
 }
--- Staff Management/Models/Tasks.cs
+++ Staff Management/Models/Tasks.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Web;
 
 namespace Staff_Management.Models
@@ -16,8 +15,6 @@ namespace Staff_Management.Models
         public String Comment { get; set; }
         public byte Status { get; set; }
         public int StaffId { get; set; }
-        public String StaffName { get; }
-
     }
 
 }

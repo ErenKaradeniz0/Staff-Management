e779978e3df3d985195471d94790fa4ecc580103
Deniz <Deniz@DESKTOP-8VVON23>
2024-05-25 21:04:18 +0300

    GroupAdminControler da Task.cshtml sayvasındaki verilerin getirilmesi için task bilgilerini çeken bir query yazıldı. Aynı view sayfasında ki elect boc için de o gruba ait çalışanların lkistesi için ayrı bir query yazıldı. Bunun için GroupAdminViewModel sayfasına gerekli olan değişkenler eklendi.

--- Staff Management/Controllers/GroupAdminController.cs
+++ Staff Management/Controllers/GroupAdminController.cs
@@ -33,6 +33,7 @@ namespace Staff_Management.Controllers
                             TaskId = task.TaskId,
                             TaskStatus = task.Status,
                             TaskTitle = task.Title,
+                            UserId = user.UserId,
                             UserName = user.Name,
                             UserSurname = user.Surname
                         };
@@ -44,10 +45,47 @@ namespace Staff_Management.Controllers
             return View(TaskList);
         }
 
+        public ActionResult Tasks(int viewTaskId) {
+             
 
+            int UserID = Convert.ToInt32(Session["UserID"]);
 
-        public ActionResult Tasks() {
-            return View();
+            var query = from assignment in _context.Assignments
+                      join user in _context.Users on assignment.StaffId equals user.UserId
+                      where assignment.GroupAdminId == UserID // Filter condition
+                      select new GroupAdminViewModel
+                      {
+                          UserId = user.UserId,
+                          UserName = user.Name,
+                          UserSurname = user.Surname
+                      };
+
+            // Convert the query result to a list
+            List<GroupAdminViewModel> ListGroupStaff = query.ToList();
+
+            var query2 = from  task in _context.Tasks 
+                        
+                        where task.TaskId == viewTaskId // Filter condition
+                        select new GroupAdminViewModel
+                        {
+                            StaffId = task.StaffId,
+                            TaskContent =task.Contents,
+                            TaskComment=task.Comment
+                            
+                        };
+            List<GroupAdminViewModel> TaskList = query2.ToList();
+
+            GroupAdminViewModel model = new GroupAdminViewModel
+            {
+                ListGroupStaff = ListGroupStaff,
+                ListTasks = TaskList
+            };
+
+            return View(model);
         }
+        
+        
+
+
     }
 }
--- Staff Management/Models/Tasks.cs
+++ Staff Management/Models/Tasks.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 
+
+
 namespace Staff_Management.Models
 {
     public class Tasks

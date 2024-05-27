41bb74ad6ca3022dba52d922bff08bc72081bed3
Eren Karadeniz <akincilareren@gmail.com>
2024-05-27 03:36:12 +0300

    SystemAdmin   Add User   Update User   Adjust Salaries

--- Staff Management/Controllers/SystemAdminController.cs
+++ Staff Management/Controllers/SystemAdminController.cs
@@ -10,30 +10,94 @@ namespace Staff_Management.Controllers
 {
     public class SystemAdminController : Controller
     {
+        private readonly DbContextViewModel _context;
+
+        public SystemAdminController()
+        {
+            _context = new DbContextViewModel();
+        }
+
         public List<Users> ListUser()
         {
-            using (var _content = new DbContextViewModel())
-            {
-                return _content.Users.ToList();
-            }
+            return _context.Users.ToList();
 
         }
-
-        // GET: SystemAdmin
         public ActionResult Index()
         {
-            
             return View(ListUser());
-
-            //return View();
-        }
-        public ActionResult AdjustSalaries()
-        {
-            return View();
         }
         public ActionResult AssignStaff()
         {
-            return View();
+            var model = new AssignStaffViewModel
+            {
+                AdminList = _context.Users.Where(u => u.Type == 2).ToList(),
+                StaffList = _context.Users.Where(u => u.Type == 3).ToList(),
+
+            };
+            return View(model);
+        }
+        public ActionResult CreateEditUser()
+        {
+            return View(ListUser());
+        }
+
+        [HttpPost]
+        public ActionResult UpdateUser(Users model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Retrieve the user from the database
+                var user = _context.Users.FirstOrDefault(u => u.UserId == model.UserId);
+
+                if (user != null)
+                {
+                    // Update user properties
+                    user.Name = model.Name;
+                    user.Surname = model.Surname;
+                    user.Title = model.Title;
+                    user.Type = model.Type;
+
+                    // Save changes to the database
+                    _context.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            // If model state is not valid or user is not found, redirect to index
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult AddUser(Users model)
+        {
+            ModelState.Remove("UserId");
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(model);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "SystemAdmin"); // Redirect to the home page, change as needed
+            }
+            return RedirectToAction("Index", "SystemAdmin");
+        }
+
+
+
+        [HttpPost]
+        public ActionResult UpdateSalary(int userId, double newSalary)
+        {
+            var user = _context.Users.Find(userId);
+            if (user != null)
+            {
+                user.Salary = newSalary;
+                _context.SaveChanges();
+                TempData["SuccessMessage"] = "Salary updated successfully";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "User not found";
+            }
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Staff Management/Models/AssignStaffViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Staff_Management.Models
+{
+    public class AssignStaffViewModel
+    {
+        public List<Users> AdminList { get; set; }
+        public List<Users> StaffList { get; set; }
+
+        public AssignStaffViewModel()
+        {
+            AdminList = new List<Users>();
+            StaffList = new List<Users>();
+        }
+
+    }
+}
